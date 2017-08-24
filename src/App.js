3888d5669a8b66cd2d0b939844a91f983f19dc39
Kirary <zskwa7@gmail.com>
2017-08-24 22:40:08 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>User Management Interface</h2>
+          <p>Интерфейс для взаимодействия с информацией о пользователях</p>
+      </div>
+    );
+  }
+}
+
+export default App;
